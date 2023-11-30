ebaef83a68e8cef9163e462eb5c0f497e6ebe192
Jordan Tran <thai.jordan@gmail.com>
2023-11-30 14:26:09 -0800

    working on org sidebar

---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const OrganizationLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+  return (
+    <main className='pt-20'>
+        {children}
+    </main>
+  )
+}
+
+export default OrganizationLayout;
