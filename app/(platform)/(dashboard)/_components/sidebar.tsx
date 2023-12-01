262172a5dee6c0870187c2a16f6083645c971aa2
Jordan Tran <thai.jordan@gmail.com>
2023-12-01 12:16:41 -0800

    sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -18,13 +18,24 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
     storageKey,
     {}
   );
+
   const { organization: activeOrganization, isLoaded: isLoadedOrg } =
     useOrganization();
+
   const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
   });
   
+  const defaultAccordionValue: string[] = Object.keys(expanded)
+  .reduce((acc: string[], key: string) => {
+    if (expanded[key]) {
+      acc.push(key);
+    }
+
+    return acc;
+}, []);
+
   return <div>sidebar</div>;
 };
