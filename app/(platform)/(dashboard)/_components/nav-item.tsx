e11f349fbe98e82d06a9072d733f52a4574097ce
Jordan Tran <thai.jordan@gmail.com>
2023-12-01 12:43:55 -0800

    nav item added

---
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const NavItem = () => {
+    return (
+        <div>
+            nav item
+        </div>
+    )
+}
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
+import { NavItem } from "./nav-item";
 
 interface SidebarProps {
   storageKey?: string;
@@ -27,15 +28,65 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
       infinite: true,
     },
   });
-  
-  const defaultAccordionValue: string[] = Object.keys(expanded)
-  .reduce((acc: string[], key: string) => {
-    if (expanded[key]) {
-      acc.push(key);
-    }
 
-    return acc;
-}, []);
+  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
+    (acc: string[], key: string) => {
+      if (expanded[key]) {
+        acc.push(key);
+      }
 
-  return <div>sidebar</div>;
+      return acc;
+    },
+    []
+  );
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id]: !expanded[id],
+    }));
+  };
+
+  if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="font-medium text-xs flex items-center mb-1">
+        <span className="pl-4">workspaces</span>
+        <Button
+          asChild
+          type="button"
+          size="icon"
+          variant="ghost"
+          className="ml-auto"
+        >
+          <Link href="/select-org">
+            <Plus className="h-4 w-4" />
+          </Link>
+        </Button>
+      </div>
+      <Accordion
+      type="multiple"
+      defaultValue={defaultAccordionValue}
+      className="space-y-2"
+      >
+        {userMemberships.data.map(({ organization }) => (
+           <NavItem 
+           key={organization.id}
+           isActive={activeOrganization?.id === organization.id}
+           isExpanded={expanded[organization.id]}
+           organization={organization}
+           onExpand={onExpand}
+           />
+        )
+        )}
+      </Accordion>
+    </>
+  );
 };
