3d49e34ca8fe706c13562264555133ac7377b78b
Jordan Tran <thai.jordan@gmail.com>
2023-12-02 12:07:10 -0800

    fixed hydration error

--- app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(platform)/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,7 +1,40 @@
-'use client'
+"use client";
+
+import { useMobileSidebar } from "@/hooks/use-mobile-sidebar";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Menu } from "lucide-react";
 
 export const MobileSidebar = () => {
-    return (
-        <div>sidebar</div>
-    )
-}
+  const pathname = usePathname();
+  const [isMounted, setIsMounted] = useState(false); // used to prevent hydration errors
+  const onOpen = useMobileSidebar((state) => state.onOpen);
+  const onClose = useMobileSidebar((state) => state.onClose);
+  const isOpen = useMobileSidebar((state) => state.isOpen);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [pathname, onClose]);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        className="block md:hidden mr-2"
+        variant="ghost"
+        size="sm"
+      >
+        <Menu className="h-4 w-4" />
+      </Button>
+    </>
+  );
+};
