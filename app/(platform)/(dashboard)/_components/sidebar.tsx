95db3bb95b8c76985fd2d24209ebc73d30e3bcd3
Jordan Tran <thai.jordan@gmail.com>
2023-12-05 13:18:26 -0800

    skeleton

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -14,31 +14,35 @@ interface SidebarProps {
   storageKey?: string;
 }
 
-export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
+export const Sidebar = ({
+  storageKey = "t-sidebar-state",
+}: SidebarProps) => {
   const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
     storageKey,
     {}
   );
 
-  const { organization: activeOrganization, isLoaded: isLoadedOrg } =
-    useOrganization();
-
-  const { userMemberships, isLoaded: isLoadedOrgList } = useOrganizationList({
+  const {
+    organization: activeOrganization,
+    isLoaded: isLoadedOrg
+  } = useOrganization();
+  const { 
+    userMemberships,
+    isLoaded: isLoadedOrgList
+  } = useOrganizationList({
     userMemberships: {
       infinite: true,
     },
   });
 
-  const defaultAccordionValue: string[] = Object.keys(expanded).reduce(
-    (acc: string[], key: string) => {
+  const defaultAccordionValue: string[] = Object.keys(expanded)
+    .reduce((acc: string[], key: string) => {
       if (expanded[key]) {
         acc.push(key);
       }
 
       return acc;
-    },
-    []
-  );
+  }, []);
 
   const onExpand = (id: string) => {
     setExpanded((curr) => ({
@@ -50,7 +54,15 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
@@ -58,7 +70,9 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   return (
     <>
       <div className="font-medium text-xs flex items-center mb-1">
-        <span className="pl-4">workspaces</span>
+        <span className="pl-4">
+          Workspaces
+        </span>
         <Button
           asChild
           type="button"
@@ -67,25 +81,26 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
           className="ml-auto"
         >
           <Link href="/select-org">
-            <Plus className="h-4 w-4" />
+            <Plus
+              className="h-4 w-4"
+            />
           </Link>
         </Button>
       </div>
       <Accordion
-      type="multiple"
-      defaultValue={defaultAccordionValue}
-      className="space-y-2"
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
       >
         {userMemberships.data.map(({ organization }) => (
-           <NavItem 
-           key={organization.id}
-           isActive={activeOrganization?.id === organization.id}
-           isExpanded={expanded[organization.id]}
-           organization={organization as Organization}
-           onExpand={onExpand}
-           />
-        )
-        )}
+          <NavItem
+            key={organization.id}
+            isActive={activeOrganization?.id === organization.id}
+            isExpanded={expanded[organization.id]}
+            organization={organization as Organization}
+            onExpand={onExpand}
+          />
+        ))}
       </Accordion>
     </>
   );
