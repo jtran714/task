d814d1d9f790a35d66d6488cd3c5d688d92e8d9d
Jordan Tran <thai.jordan@gmail.com>
2023-12-07 15:15:05 -0800

    added popover to navbar comp

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,31 +1,28 @@
+import { Plus } from "lucide-react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { Plus } from "lucide-react";
+import { FormPopover } from "@/components/form/form-popover";
 import { MobileSidebar } from "./mobile-sidebar";
 
-export const NavBar = () => {
+export const Navbar = () => {
   return (
-    <nav className="fixed z-50 top-0 w-full px-4 h-14 border-b shadow-sm bg-white flex items-center">
+    <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
       <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
         </div>
-        <Button
-          size="sm"
-          className="rounded-sm hidden md:block h-auto py-1.5 px-2"
-          variant="primary"
-        >
-          Create
-        </Button>
-        <Button
-          size="sm"
-          className="rounded-sm block md:hidden"
-          variant="primary"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" side="bottom" sideOffset={18}>
+          <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto  py-1.5 px-2">
+            Create
+          </Button>
+        </FormPopover>
+        <FormPopover>
+          <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="ml-auto flex items-center gap-x-2">
         <OrganizationSwitcher
@@ -50,8 +47,8 @@ export const NavBar = () => {
               avatarBox: {
                 height: 30,
                 width: 30,
-              },
-            },
+              }
+            }
           }}
         />
       </div>
