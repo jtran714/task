070cfa6934a3526b85368aac78aa1f3d627644f8
Jordan Tran <thai.jordan@gmail.com>
2023-12-08 15:37:43 -0800

    misc

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -7,8 +7,8 @@ import { ListWithCards } from "@/types";
 import { useAction } from "@/hooks/use-action";
 import { updateListOrder } from "@/actions/update-list-order";
 import { updateCardOrder } from "@/actions/update-card-order";
-import { ListForm } from "./list-form";
-import { ListItem } from "./list-item";
+// import { ListForm } from "./list-form";
+// import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
