e32026639b9fa40fa86b8f4d34bd9319cd84798d
Jordan Tran <thai.jordan@gmail.com>
2023-12-08 15:38:59 -0800

    misc again

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,13 +2,13 @@
 
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
-// import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
 import { ListWithCards } from "@/types";
 import { useAction } from "@/hooks/use-action";
 import { updateListOrder } from "@/actions/update-list-order";
 import { updateCardOrder } from "@/actions/update-card-order";
-// import { ListForm } from "./list-form";
-// import { ListItem } from "./list-item";
+import { ListForm } from "./list-form";
+import { ListItem } from "./list-item";
 
 interface ListContainerProps {
   data: ListWithCards[];
