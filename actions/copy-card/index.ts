116140d1427821a5f7206badcea2349f615e3813
Jordan Tran <thai.jordan@gmail.com>
2023-12-12 14:14:17 -0800

    copy list done and cleaned up copy card

--- actions/copy-card/index.ts
+++ actions/copy-card/index.ts
@@ -3,11 +3,9 @@
 import { auth } from "@clerk/nextjs";
 import { revalidatePath } from "next/cache";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
-
 import { db } from "@/lib/db";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
-
 import { CopyCard } from "./schema";
 import { InputType, ReturnType } from "./types";
 
